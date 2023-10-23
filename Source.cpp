1c4a04d6d2beb33784ef6293f55c03571323dadf
khanh <hanhatkhanh2282k2star@gmail.com>
2023-10-23 14:21:07 +0700

    add tich

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
 
 using namespace std;
-<<<<<<< HEAD
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b){
-	int tich = 0;
-	return tich = a*b;
-}
-float thuong(int a, int b);
-int main()
-{
-	int a, int b;
-	cout<< "Nhap a";
-	cin>>a;
-	cout<<"Nhap b";
-	cin>>b;
-	tich(a,b);
-=======
 int hieu(int a, int b){
 	int hieu = 0;
 	return hieu = a - b;
@@ -27,19 +10,22 @@ int tong(int a, int b){
 	sum = a + b;
 	return sum;
 }
-int tich(int a, int b);
+int tich(int a, int b){
+	int tich = 0;
+	return tich = a * b ;
+}
 float thuong(int a, int b);
 int main()
 {
 	int a , int b;
 	
 	
->>>>>>> 7df6c7c8680ab25cc62fc9568e67741224d40a1b
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Nhap a: "; cin >> a;
 	cout <<" Nhap b: "; cin >> b;
 	cout << "Tong la" <<tong(a,b)<<endl;
 	cout << "HIEU A VA B" << hieu(a,b)<<endl;
+	cout << " tich la " << tich (a,b)<<endl;
 	system("pause");
 	return 0;
 }
