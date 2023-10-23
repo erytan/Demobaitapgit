ea2c4cf94ad2f7562bf75be81c36a5c295b09387
thinhle2002 <thinhle15112002@gmail.com>
2023-10-23 14:03:38 +0700

    update ham tich

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,19 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	int tich = 0;
+	return tich = a*b;
+}
 float thuong(int a, int b);
 int main()
 {
+	int a, int b;
+	cout<< "Nhap a";
+	cin>>a;
+	cout<<"Nhap b";
+	cin>>b;
+	tich(a,b);
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	system("pause");
 	return 0;
