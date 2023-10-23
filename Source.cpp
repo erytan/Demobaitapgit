69fd625990f9cd43e290a7bde7612e84acc72b3c
khanh <hanhatkhanh2282k2star@gmail.com>
2023-10-23 13:56:17 +0700

    update ham

--- Source.cpp
+++ Source.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	int hieu = 0;
+	return hieu = a - b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a , int b;
+	cout << a;
+	cin >> a;
+	cout << b;
+	cin >> b;
+	cout << "HIEU A VA B" << hieu(a,b)<<endl;
+	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	system("pause");
 	return 0;
