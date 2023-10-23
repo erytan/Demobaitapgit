4e64232351f6b5f0e75830699840b40283e33ca9
erytan <ntan1695123@gmail.com>
2023-10-23 13:54:16 +0700

    update tong

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	int sum = 0;
+	sum = a + b;
+	return sum;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a,b;
+
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout << "Nhap a: "; cin >> a;
+	cout <<" Nhap b: "; cin >> b;
+	cout << "Tong la" <<tong(a,b);
 	system("pause");
 	return 0;
 }
