bde68d2ca18802f896bd218ccf9ad821d957c9cd
Duy <trleminhduy@gmail.com>
2023-10-30 13:40:51 +0700

    Add thuong

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b){
+	return a/b
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
@@ -16,7 +18,7 @@ int main()
 	cout<<a<<" + "<<b<<" = "<<tong(a+b)<<endl;
 	cout<<a<<" - "<<b<<" = "<<hieu(a+b)<<endl;
 	cout<<a<<" * "<<b<<" = "<<tich(a+b)<<endl;
-	cout<<a<<" / "<<b<<" = "<<thuong(a+b)<<endl;
+	cout<<a<<" / "<<b<<" = "<<thuong(a/b)<<endl;
 	system("pause");
 	return 0;
 }
